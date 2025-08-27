251bb97730ac69f7223b3113ea148fcbfc2ee908
yeonhee2 <qodusgml03@naver.com>
2025-08-27 16:31:09 +0900

    post게시판 페이지화, 검색, 수정, 삭제,등록 기능 구현

--- src/main/java/com/example/mbcBoard/service/PostService.java
+++ src/main/java/com/example/mbcBoard/service/PostService.java
@@ -1,8 +1,14 @@
 package com.example.mbcBoard.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.example.mbcBoard.domain.Post;
 import com.example.mbcBoard.repository.PostRepository;
 
 @Service
@@ -11,4 +17,49 @@ public class PostService {
 	@Autowired
 	private PostRepository postRepository;
 	
+	public void insertPost(Post post) {
+		postRepository.save(post);
+	}
+	
+	@Transactional(readOnly = true)
+	public Page<Post> getPostList(Pageable pageable){
+		return postRepository.findAll(pageable);
+		
+	}
+	
+	public Post getPost(int id) {
+		return postRepository.findById(id).get();
+	}
+	
+	@Transactional
+	public void updatePost(Post post) {
+		Post update = getPost(post.getId());
+		
+		update.setTitle(update.getTitle());
+		update.setContent(update.getContent());
+		postRepository.save(update);
+	}
+	
+	public void deletePost(int id) {
+		postRepository.deleteById(id);
+	}
+	
+	public Page<Post> searchPost(String type, String keyword, int page, int size) {
+		// PageRequest : 데이터를 페이지 단위로 검색하고 결과를 제어하는데 사용하는 객체
+		// page : 몇 번째 페이지를 요청하는 지
+		// size : 한 페이지에 몇개의 데이터를 가져올지
+		// Sort.by("id").descending() : 정렬 기준과 방향
+		Pageable pageable = PageRequest.of(page, size,Sort.by("id").descending());
+		
+		switch (type) {
+			case "title" :
+				return postRepository.findByTitleContaining(keyword, pageable);
+			case "content" :
+				return postRepository.findByContentContaining(keyword, pageable);
+			case "username" :
+				return postRepository.findByUsernameContaining(keyword, pageable);
+			default:
+				return Page.empty();
+		}
+	}
 }
